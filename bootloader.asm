0c49727b6286421a57ed4ece2e039f13bcddfafa
Arthur <arthur.daes@gmail.com>
2024-05-18 02:36:13 -0300

    improvment to the hello world

--- bootloader.asm
+++ bootloader.asm
@@ -1,29 +1,21 @@
-BITS 16
-ORG 0x7c00
+bits 16
+org 0x7c00
 
 start:
   mov si, message
-  call print
-  jmp $
-
-print:
-  mov bx, 0
-.loop:
-  lodsb
-  cmp al, 0
-  je .done
-  call print_char
-  jmp .loop
-
-.done:
-  ret
 
 print_char:
-  mov ah, 0eh
-  int 0x10
-  ret
+  lodsb           ; putting the first byte of SI into AL
+  cmp al, 0       ; checking for null terminator string
+  je done         ; end
+  mov ah, 0eh     ; teletype 
+  int 0x10      
+  jmp print_char  ; going for the next character
 
-message: db 'Bootloader!', 0
+done:
+  hlt
+
+message: db 'Hello Bootloader!', 0
 
 times 510-($ - $$) db 0
 dw 0xAA55
